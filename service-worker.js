b482a04fa4f285851ce213869f5afe9f82d98ac9
DanBudszzz <tdanbuds@gmail.com>
2025-08-02 00:39:50 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+// A simple service worker for caching main assets for offline use.
+const CACHE_NAME = 'nursing-hub-cache-v1';
+const urlsToCache = [
+  './',
+  'index.html',
+  'https://olivarezcollegetagaytay.edu.ph/img/olivarez-college-tagaytay-logo.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      // If the request is in the cache, return it. Otherwise, fetch from the network.
+      return response || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
